aff04c58e1bfeddc76e3c7ec02682f515d1c1266
Yousif627 <yousifabdulaziz18@gmail.com>
2025-07-13 00:58:12 -0700

    update

---
+++ scripts/app.js
@@ -0,0 +1,31 @@
+function init() {
+
+
+const sqrElem = document.querySelector('.sqr')
+
+
+
+
+
+
+
+function handleClick(){
+
+}
+
+
+
+
+
+
+
+
+
+
+sqrElem.foreach((yes)=>{
+    yes.addEventListener('click', handleClick)
+})
+
+}
+
+document.addEventListener('DOMContentLoaded', init)
