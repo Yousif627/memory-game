ea982744f3e37103f3aabc298bee8ffff7b55b2e
Yousif627 <yousifabdulaziz18@gmail.com>
2025-07-15 00:00:45 -0700

    pop up btn for how to play the game

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,116 @@
 function init() {
+    const cardElem = document.querySelectorAll(".memory-card")
+    const timerElm = document.querySelector("#timer")
+    const messageElm = document.querySelector(".message")
+    const openBtn = document.getElementById("openModal")
+    const closeBtn = document.getElementById("closeModal")
+    const modalElm = document.getElementById("modal")
+
+    let Flipped = false;
+    let firstCard, secondCard;
+    let blocked = false;
+    let matches = 0;
+    let timer = 40;
+    let interval;
 
 
-const sqrElem = document.querySelectorAll('.sqr')
-const flipCard = document.querySelector('.divisions')
+
+function flipCard() {
+        if (blocked) return;
 
 
-const checkList = [],[],[],[],[],[]
+        this.classList.add('flip');
+
+        if (!Flipped) {
+            console.log(firstCard)
+            Flipped = true;
+            firstCard = this;
+            // timers()
+            return;
+        }
+
+        Flipped = false;
+        secondCard = this;
+        console.log(secondCard)
+
+        matching()
+ }
+    // console.log(firstCard.dataset.framework)
+    // console.log(secondCard.dataset.framework)
 
 
-function handleClick(){
-    console.log('you clicked me')
+function matching() {
+        console.log("matching function called")
+        console.log(firstCard.dataset.framework)
+        console.log(secondCard.dataset.framework)
+        let match = firstCard.dataset.framework === secondCard.dataset.framework;
+        console.log(match)
+        match ? disableCard() : dontFlip(), matches++;
 
 }
 
+function disableCard() {
+        firstCard.removeEventListener('click', flipCard)
+        secondCard.removeEventListener('click', flipCard)
+}
+
+function dontFlip() {
+        blocked = true;
+        firstCard.classList.remove('flip')
+        secondCard.classList.remove('flip')
+        blocked = false;
+}
+
+(function shuffle() {
+        cardElem.forEach(card => {
+            let randomize = Math.floor(Math.random() * 12);
+            card.style.order = randomize;
+        });
+})();
 
 
 
-flipCard.addEventListener("click",()=>{
-    flipCard.classList.toggle("clicked")
+cardElem.forEach(card => {
+        card.addEventListener('click', flipCard)
+});
+
+openBtn.addEventListener("click", () => {
+        modalElm.classList.add("open");
 })
 
-sqrElem.forEach(yes => {
-    yes.addEventListener('click', handleClick)
-});
+closeBtn.addEventListener("click", () => {
+        modalElm.classList.remove("open");
+})
+
+function timers() {
+        interval = setInterval(() => {
+            timer--
+            timerElm.textContent = timer
+            console.log()
+            if (timer === 0) {
+                stopWatch()
+            }
+            // if(timerElm.textContent === || matches!==6){
+            //     clearInterval(interval)
+            //     messageElm.textContent = 'Time is up loser!'
+            // }
+        }, 1000)
 }
 
+ function stopWatch() {
+        clearInterval(interval);
+
+ }
+
+
+
+
+    // if (timer === 0){
+
+    //                 messageElm.textContent = 'Time is up loser!'
+    //                 console.log(timers)
+    //             }
+    timers()
+
+}
 document.addEventListener('DOMContentLoaded', init)
